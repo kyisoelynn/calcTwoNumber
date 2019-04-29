2fc67d1755aef18d864f5d4a09c3f3698ed36dd4
heinminhtet007 <heinminhtet8138@gmail.com>
2019-04-29 16:51:46 +0630

    calc android

---
+++ app/src/main/java/com/hein/calc/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hein.calc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void addNumber(){}
+}
